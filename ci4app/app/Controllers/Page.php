0a6fe980a750dbb15e35deba97d29e127ba095e7
rizkymukarramah <57203841+rizkymukarramah@users.noreply.github.com>
2022-04-28 13:12:14 +0700

    enter

---
+++ ci4app/app/Controllers/Page.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        echo view("about_page");
+    }
+
+    public function contact()
+    {
+        echo view("contact_page");
+    }
+
+    public function products()
+    {
+        echo view("product_page");
+    }
+
+    public function drivers()
+    {
+        echo view("drivers_page");
+    }
+
+    public function technologies()
+    {
+        echo view("technologies_page");
+    }
+}
