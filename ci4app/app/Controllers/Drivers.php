29ca7a1f7d9e661f4c3c6ffd0af210485481d1ac
rizkymukarramah <57203841+rizkymukarramah@users.noreply.github.com>
2022-04-28 12:27:36 +0700

    first commit

---
+++ ci4app/app/Controllers/Drivers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Drivers extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+}
---
+++ ci4app/app/Controllers/Technologies.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Technologies extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+}
